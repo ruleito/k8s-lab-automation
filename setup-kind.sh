33aad3387293395c2770bb3b4f3d883c8c3c2537
denis.kurdyaev <denis.kurdyaev@inplatlabs.ru>
2026-01-12 23:03:07 +0300

    upd sh

--- setup-kind.sh
+++ setup-kind.sh
@@ -13,6 +13,8 @@ if ! command -v minikube &>/dev/null; then
   curl -Lo minikube https://github.com/kubernetes/minikube/releases/download/v1.37.0/minikube-darwin-arm64
   chmod +x minikube
   sudo mv minikube /usr/local/bin/
+  curl -fsSL https://github.com/minikube-machine/vmnet-helper/releases/latest/download/install.sh | bash
+  brew upgrade vfkit
 fi
 
 echo "=== 1. starting minikube cluster ==="
